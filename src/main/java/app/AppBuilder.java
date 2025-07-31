92114e7184eb43e17053ad46eafa49c20d508d45
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2025-07-30 22:05:18 -0400

    fix

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -59,166 +59,254 @@ import view.*;
  * This is done by adding each View and then adding related Use Cases.
  */
 public class AppBuilder {
-    private final JPanel cardPanel = new JPanel();
-    private final CardLayout cardLayout = new CardLayout();
-    private final UserFactory userFactory = new CommonUserFactory();
-    private final ViewManagerModel viewManagerModel = new ViewManagerModel();
-    private final ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
-    private final LoginUserDataAccessInterface userDataAccessObject =
-            new DBUserDataAccessObject(userFactory);
-    private final DBPortfoliosDataAccessObject portfoliosDataAccessObject = new DBPortfoliosDataAccessObject();
-    private final DBPortfolioDataAccessObject portfolioDataAccessObject = new DBPortfolioDataAccessObject();
+        private final JPanel cardPanel = new JPanel();
+        private final CardLayout cardLayout = new CardLayout();
+        private final UserFactory userFactory = new CommonUserFactory();
+        private final ViewManagerModel viewManagerModel = new ViewManagerModel();
+        private final ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
+        private final LoginUserDataAccessInterface userDataAccessObject = new DBUserDataAccessObject(userFactory);
+        private final DBPortfoliosDataAccessObject portfoliosDataAccessObject = new DBPortfoliosDataAccessObject();
+        private final DBTransactionDataAccessObject transactionDataAccessObject = new DBTransactionDataAccessObject();
+        private final DBStockDataAccessObject stockDataAccessObject = new DBStockDataAccessObject();
 
-    private SignupView signupView;
-    private SignupViewModel signupViewModel;
-    private LoginViewModel loginViewModel;
-    private MainViewModel mainViewModel;
-    private PortfoliosViewModel portfoliosViewModel;
-    private CreateViewModel createViewModel;
-    private PortfolioViewModel portfolioViewModel;
-    private MainView mainView;
-    private LoginView loginView;
-    private PortfoliosView portfoliosView;
-    private CreateView createView;
-    private PortfolioView portfolioView;
+        private final MainViewModel mainViewModel = new MainViewModel();
+        private final LoginViewModel loginViewModel = new LoginViewModel();
+        private final SignupViewModel signupViewModel = new SignupViewModel();
+        private final PortfoliosViewModel portfoliosViewModel = new PortfoliosViewModel();
+        private final NewsViewModel newsViewModel = new NewsViewModel();
+        private final CreateViewModel createViewModel = new CreateViewModel();
+        private final PortfolioViewModel portfolioViewModel = new PortfolioViewModel();
+        private final BuyViewModel buyViewModel = new BuyViewModel();
+        private final SellViewModel sellViewModel = new SellViewModel();
+        private final HistoryViewModel historyViewModel = new HistoryViewModel();
+        private final AnalysisViewModel analysisViewModel = new AnalysisViewModel();
+        private final RecommendViewModel recommendViewModel = new RecommendViewModel();
+        private final PortfolioViewModelUpdater portfolioViewModelUpdater = new PortfolioViewModelUpdater();
+        private final NavigationState navigationState = new NavigationState();
+        private final NavigationOutputBoundary navigationPresenter = new NavigationPresenter(viewManagerModel);
+        private final NavigationInteractor navigationInteractor = new NavigationInteractor(navigationState,
+                        navigationPresenter);
+        private final NavigationController navigationController = new NavigationController(navigationInteractor);
+
+        private final LoginController loginController = LoginUseCaseFactory.create(
+                        viewManagerModel,
+                        mainViewModel,
+                        loginViewModel,
+                        signupViewModel,
+                        userDataAccessObject);
+        private final SignupController signupController = SignupUseCaseFactory.create(
+                        viewManagerModel,
+                        signupViewModel,
+                        loginViewModel,
+                        (SignupUserDataAccessInterface) userDataAccessObject);
+        private final PortfoliosController portfoliosController = PortfoliosUseCaseFactory.create(
+                        viewManagerModel,
+                        portfoliosViewModel,
+                        createViewModel,
+                        portfoliosDataAccessObject);
+
+        private final CreateController createController = CreateUseCaseFactory.create(
+                        viewManagerModel,
+                        portfoliosViewModel,
+                        createViewModel,
+                        portfoliosDataAccessObject);
+
+        private final NewsController newsController = NewsUseCaseFactory.create(
+                        viewManagerModel,
+                        newsViewModel,
+                        transactionDataAccessObject);
+        private final PortfolioController portfolioController = PortfolioUseCaseFactory.create(
+                        viewManagerModel,
+                        portfolioViewModel,
+                        buyViewModel,
+                        sellViewModel,
+                        transactionDataAccessObject,
+                        stockDataAccessObject,
+                        navigationController);
+
+        private final BuyController buyController = BuyUseCaseFactory.create(
+                        viewManagerModel,
+                        buyViewModel,
+                        portfolioViewModelUpdater,
+                        portfolioViewModel,
+                        stockDataAccessObject,
+                        transactionDataAccessObject);
+
+        private final SellController sellController = SellUseCaseFactory.create(
+                        viewManagerModel,
+                        sellViewModel,
+                        portfolioViewModel,
+                        portfolioViewModelUpdater,
+                        stockDataAccessObject,
+                        transactionDataAccessObject);
+
+        private final HistoryController historyController = HistoryUseCaseFactory.create(
+                        viewManagerModel,
+                        historyViewModel,
+                        transactionDataAccessObject,
+                        navigationController);
+
+        private final AnalysisController analysisController = AnalysisUseCaseFactory.create(
+                        viewManagerModel,
+                        analysisViewModel,
+                        stockDataAccessObject,
+                        transactionDataAccessObject,
+                        navigationController);
+
+        private final RecommendController recommendController = RecommendUseCaseFactory.create(
+                        viewManagerModel,
+                        recommendViewModel,
+                        stockDataAccessObject,
+                        transactionDataAccessObject);
+
+        private MainView mainView;
+        private LoginView loginView;
+        private SignupView signupView;
+        private PortfoliosView portfoliosView;
+        private CreateView createView;
+        private PortfolioView portfolioView;
+        private NewsView newsView;
+        private BuyView buyView;
+        private SellView sellView;
+        private HistoryView historyView;
+        private AnalysisView analysisView;
+        private RecommendView recommendView;
 
         public AppBuilder() throws SQLException {
                 cardPanel.setLayout(cardLayout);
         }
 
-    /**
-     * Adds the Login View to the application.
-     * @return this builder
-     */
-    public AppBuilder addLoginView() {
-        loginViewModel = new LoginViewModel();
-        loginView = new LoginView(loginViewModel);
-        cardPanel.add(loginView, loginView.getViewName());
-        return this;
-    }
+        /**
+         * Adds the Main View to the application.
+         * 
+         * @return this builder
+         */
+        public AppBuilder addMainView() {
+                mainView = MainViewFactory.create(mainViewModel, portfoliosController, newsController,
+                                navigationController);
+                cardPanel.add(mainView, mainView.getViewName());
+                return this;
+        }
 
-    /**
-     * Adds the Signup View to the application.
-     * @return this builder
-     */
-    public AppBuilder addSignupView() {
-        signupViewModel = new SignupViewModel();
-        signupView = new SignupView(signupViewModel);
-        cardPanel.add(signupView, signupView.getViewName());
-        return this;
-    }
+        public AppBuilder addLoginView() {
+                loginView = LoginViewFactory.create(loginViewModel, loginController);
+                cardPanel.add(loginView, loginView.getViewName());
+                return this;
+        }
 
-    /**
-     * Adds the Main View to the application.
-     * @return this builder
-     */
-    public AppBuilder addMainView() {
-        mainViewModel = new MainViewModel();
-        mainView = new MainView(mainViewModel);
-        cardPanel.add(mainView, mainView.getViewName());
-        return this;
-    }
+        public AppBuilder addSignupView() {
+                signupView = SignupViewFactory.create(signupViewModel, signupController);
+                cardPanel.add(signupView, signupView.getViewName());
+                return this;
+        }
 
-    /**
-     * Adds the portfolios view to the application
-     * @return this builder
-     */
-    public AppBuilder addPortfoliosView() {
-        portfoliosViewModel = new PortfoliosViewModel();
-        portfoliosView = new PortfoliosView(portfoliosViewModel);
-        cardPanel.add(portfoliosView, portfoliosView.getViewName());
-        return this;
-    }
+        /**
+         * Adds the portfolios view to the application
+         * 
+         * @return this builder
+         */
+        public AppBuilder addPortfoliosView() {
+                portfoliosView = PortfoliosViewFactory.create(
+                                portfoliosViewModel,
+                                portfoliosController,
+                                portfolioController,
+                                navigationController);
+                cardPanel.add(portfoliosView, portfoliosView.getViewName());
+                return this;
+        }
 
-    /**
-     * Adds the create view to the application
-     * @return this builder
-     */
-    public AppBuilder addCreateView() {
-        createViewModel = new CreateViewModel();
-        createView = new CreateView(createViewModel);
-        cardPanel.add(createView, createView.getViewName());
-        return this;
-    }
+        /**
+         * Adds the create view to the application
+         * 
+         * @return this builder
+         */
+        public AppBuilder addCreateView() {
+                createView = CreateViewFactory.create(
+                                createViewModel,
+                                createController);
+                cardPanel.add(createView, createView.getViewName());
+                return this;
+        }
 
-    /**
-     * Adds the portfolio view to the application
-     * @return this builder
-     */
-    public AppBuilder addPortfolioView() {
-        portfolioViewModel = new PortfolioViewModel();
-        portfolioView = new PortfolioView(portfolioViewModel);
-        cardPanel.add(portfolioView, portfolioView.getViewName());
-        return this;
-    }
+        /**
+         * Adds the portfolio view to the application
+         * 
+         * @return this builder
+         */
+        public AppBuilder addPortfolioView() {
+                portfolioView = PortfolioViewFactory.create(
+                                portfolioViewModel,
+                                portfolioController,
+                                historyController,
+                                analysisController,
+                                recommendController,
+                                navigationController);
+                cardPanel.add(portfolioView, portfolioView.getViewName());
+                return this;
+        }
 
-    /**
-     * Adds the Login Use Case to the application.
-     * @return this builder
-     */
-    public AppBuilder addLoginUseCase() {
-        final LoginOutputBoundary loginOutputBoundary = new LoginPresenter(viewManagerModel,
-                mainViewModel, loginViewModel);
-        final LoginInputBoundary loginInteractor = new LoginInteractor(
-                userDataAccessObject, loginOutputBoundary);
+        public AppBuilder addNewsView() {
+                newsView = new NewsView(newsViewModel, navigationController);
+                cardPanel.add(newsView, newsView.getViewName());
+                return this;
+        }
 
-        final LoginController loginController = new LoginController(loginInteractor);
-        loginView.setLoginController(loginController);
-        return this;
-    }
+        /**
+         * Adds the buy view to the application
+         * 
+         * @return this builder
+         */
+        public AppBuilder addBuyView() {
+                buyView = BuyViewFactory.create(
+                                buyViewModel,
+                                buyController,
+                                navigationController);
+                cardPanel.add(buyView, buyView.getViewName());
+                return this;
+        }
 
+        public AppBuilder addSellView() {
+                sellView = SellViewFactory.create(sellViewModel, sellController, navigationController);
+                cardPanel.add(sellView, sellView.getViewName());
+                return this;
+        }
 
-    /**
-     * Adds the Signup Use Case to the application.
-     * @return this builder
-     */
-    public AppBuilder addSignupUseCase() {
-        final SignupOutputBoundary signupOutputBoundary = new SignupPresenter(viewManagerModel,
-                signupViewModel, loginViewModel);
-        final SignupInputBoundary userSignupInteractor = new SignupInteractor(
-                (SignupUserDataAccessInterface) userDataAccessObject, signupOutputBoundary, userFactory);
+        public AppBuilder addHistoryView() {
+                historyView = HistoryViewFactory.create(
+                                historyViewModel,
+                                historyController,
+                                navigationController);
+                cardPanel.add(historyView, historyView.getViewName());
+                return this;
+        }
 
-        final SignupController controller = new SignupController(userSignupInteractor);
-        signupView.setSignupController(controller);
-        return this;
-    }
+        public AppBuilder addAnalysisView() {
+                analysisView = AnalysisViewFactory.create(
+                                analysisViewModel,
+                                analysisController,
+                                navigationController);
+                cardPanel.add(analysisView, analysisView.getViewName());
+                return this;
+        }
 
+        public AppBuilder addRecommendView() {
+                recommendView = RecommendViewFactory.create(
+                                recommendViewModel,
+                                recommendController,
+                                navigationController);
+                cardPanel.add(recommendView, recommendView.getViewName());
+                return this;
+        }
 
-    public AppBuilder addPortfoliosUseCase() {
-        final PortfoliosOutputBoundary portfoliosOutputBoundary = new PortfoliosPresenter(viewManagerModel,
-                portfoliosViewModel, createViewModel);
-        final PortfoliosInputBoundary portfoliosInteractor = new PortfoliosInteractor(portfoliosOutputBoundary,
-                portfoliosDataAccessObject);
-        final PortfoliosController portfoliosController = new PortfoliosController(portfoliosInteractor);
-        mainView.setController(portfoliosController);
-        portfoliosView.setPortfoliosController(portfoliosController);
-        return this;
-
-    }
-
-    public AppBuilder addCreateUseCase() {
-        final CreateOutputBoundary createOutputBoundary = new CreatePresenter(viewManagerModel, portfoliosViewModel, createViewModel);
-        final CreateInputBoundary createInteractor = new CreateInteractor(portfoliosDataAccessObject, createOutputBoundary);
-        final CreateController createController = new CreateController(createInteractor);
-        createView.setController(createController);
-        return this;
-    }
-
-    public AppBuilder addPortfolioUseCase() {
-        final PortfolioOutputBoundary portfolioOutputBoundary = new PortfolioPresenter(viewManagerModel, portfolioViewModel);
-        final PortfolioInputBoundary portfolioInputBoundary = new PortfolioInteractor(portfolioDataAccessObject, portfolioOutputBoundary);
-        final PortfolioController controller = new PortfolioController(portfolioInputBoundary);
-        portfoliosView.setPortfolioController(controller);
-        return this;
-    }
-
-    /**
-     * Creates the JFrame for the application and initially sets the SignupView to be displayed.
-     * @return the application
-     */
-    public JFrame build() {
-        final JFrame application = new JFrame("FUNDI");
-        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        /**
+         * Creates the JFrame for the application and initially sets the SignupView to
+         * be displayed.
+         * 
+         * @return the application
+         */
+        public JFrame build() {
+                final JFrame application = new JFrame("FUNDI");
+                application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
                 application.add(cardPanel);
 

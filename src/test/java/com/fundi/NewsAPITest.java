bb63276c1ca6a2f13a0820117d66eaa3e09ca840
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2025-07-31 01:25:54 -0400

    Ensure readers close

--- src/test/java/com/fundi/NewsAPITest.java
+++ src/test/java/com/fundi/NewsAPITest.java
@@ -8,14 +8,16 @@ import java.io.*;
 public class NewsAPITest {
     public static void main(String[] args) throws IOException {
         File file = new File("data/news_key.txt");
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String key = reader.readLine().trim();
-        System.out.println(key);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String key = reader.readLine().trim();
+            System.out.println(key);
 
-        String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + key;
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(url).build();
-        Response response = client.newCall(request).execute();
-        System.out.println(response.body().string());
+            String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + key;
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(url).build();
+            try (Response response = client.newCall(request).execute()) {
+                System.out.println(response.body().string());
+            }
+        }
     }
 }
